bf1fbe412a118c012a6bc6c8d1aa06ca8c13c118
xdxdVSxdxd <xdxd.vs.xdxd@gmail.com>
2015-05-06 21:54:37 +0200

    Added the first extraction programs in PHP

---
+++ PHP/getFirstOSMAVizData.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once('db.php');
+require_once('prepareStatements.php');
+
+$res = array();
+
+$q1 = "SELECT lat,lng, count(*) as c FROM content WHERE city='" . $citycode . "' AND ( NOT lat=999 OR NOT lng=999 ) GROUP BY lat, lng ORDER BY c DESC";
+
+$r1 = $dbh->query($q1);
+if($r1){
+	foreach ( $r1 as $row1) {
+		
+       
+
+		$r = array();
+
+		$r["lat"] = $row1["lat"];
+		$r["lng"] = $row1["lng"];
+		$r["c"] = $row1["c"];
+
+		$res[] = $r;
+	}
+}
+
+
+echo( json_encode($res) );
+
+?>
